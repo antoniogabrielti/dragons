fad4c91298ed98e4cb6b74ee84da650c47238aeb
João Vitor <leand.joao@outlook.com.br>
2021-04-29 23:31:54 -0300

    final form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import './global.scss';
+
+import Login from './views/Login/';
+import Dragons from './views/Dragons/';
+import EditDragon from './views/EditDragon/';
+
+const loggedIn = localStorage.getItem('loggedIn');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          { loggedIn ? <Redirect to="dragons" /> : <Login /> }
+        </Route>
+        <Route exact path="/dragons">
+        { loggedIn ? <Dragons /> : <Redirect to="/" />}
+        </Route>
+        <Route exact path="/dragons/edit/:dragonId">
+        { loggedIn ? <EditDragon /> : <Redirect to="/" />}
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
